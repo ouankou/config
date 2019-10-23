ed59e35d42f868bf040d6801f034bfff6c8a80d0
Anjia Wang <anjiawang@gmail.com>
2019-10-22 22:53:06 -0400

    Update setup script.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@
 # setup autostart before login
 # copy rc.local to /etc/
 
+git config --global user.name "Anjia Wang"
+git config --global user.email "anjiawang@gmail.com"
+git config --global commit.gpgsign true
+git config --global gpg.program gpg2
+#git config --global user.signingkey

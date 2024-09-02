3c40c9992a8e5bbbf0db51b8c2f08f034ad24a82
Anjia Wang <anjiawang@gmail.com>
2024-09-02 23:42:14 +0000

    Update the script for install Nvidia HPC toolkit

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda-repo-ubuntu2004-11-1-local_11.1.1-455.32.00-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-1-local_11.1.1-455.32.00-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-1-local/7fa2af80.pub
-sudo apt update
-sudo apt install -y cuda
-
+wget https://developer.download.nvidia.com/hpc-sdk/24.3/nvhpc_2024_243_Linux_x86_64_cuda_12.3.tar.gz
+tar xpzf nvhpc_2024_243_Linux_x86_64_cuda_12.3.tar.gz
+sudo nvhpc_2024_243_Linux_x86_64_cuda_12.3/install
